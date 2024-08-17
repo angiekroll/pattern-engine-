dda99ffff0c5230f43932b52dcd41fc778b46ca7
Angela Castillo <ancastillo@novopayment.com>
2024-08-17 17:28:53 -0500

    ✨ feat(patter-engine): initial commit

---
+++ src/main/java/com/appgate/pattern_engine/PatternEngineApplication.java
@@ -0,0 +1,13 @@
+package com.appgate.pattern_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatternEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatternEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/appgate/pattern_engine/PatternEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appgate.pattern_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatternEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
