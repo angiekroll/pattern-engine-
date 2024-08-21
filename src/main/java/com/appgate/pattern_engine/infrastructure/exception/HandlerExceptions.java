3b5148627a69e74955b06d84e175910b827a429d
Angela Castillo <ancastillo@novopayment.com>
2024-08-21 14:28:56 -0500

    ✨ feat(patter-engine): fix handler exception

--- src/main/java/com/appgate/pattern_engine/infrastructure/exception/HandlerExceptions.java
+++ src/main/java/com/appgate/pattern_engine/infrastructure/exception/HandlerExceptions.java
@@ -3,16 +3,17 @@
  */
 package com.appgate.pattern_engine.infrastructure.exception;
 
-import com.appgate.pattern_engine.infrastructure.constant.NotificationCode;
-import com.appgate.pattern_engine.infrastructure.dto.Notification;
+import com.appgate.pattern_engine.infrastructure.dto.response.NotificationError;
+import com.appgate.pattern_engine.infrastructure.dto.response.ValidationError;
 import jakarta.validation.ConstraintViolationException;
 import java.time.Instant;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -32,7 +33,7 @@ public class HandlerExceptions {
   public ResponseEntity<Object> handlerPaymentPlatformException(PatterEngineException ex) {
     log.error("Exception: [{}]", ex.getErrorCode().getDescription(), ex);
     return new ResponseEntity<>(
-        Notification.builder()
+        NotificationError.builder()
             .message(ex.getErrorCode().getDescription())
             .status(ex.getErrorCode().getHttpStatus().value())
             .error(ex.getErrorCode().getHttpStatus().getReasonPhrase())
@@ -42,11 +43,11 @@ public class HandlerExceptions {
   }
 
   @ExceptionHandler(MissingServletRequestParameterException.class)
-  public ResponseEntity<Notification> handlerMissingServletRequestParameterException(
+  public ResponseEntity<NotificationError> handlerMissingServletRequestParameterException(
       Exception ex) {
     log.error("Incorrect parameters: {}", ex.getMessage(), ex);
     return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-        .body(Notification.builder()
+        .body(NotificationError.builder()
             .message(
                 "Incorrect parameters. " + ex.getMessage())
             .status(HttpStatus.BAD_REQUEST.value())
@@ -56,19 +57,30 @@ public class HandlerExceptions {
   }
 
   @ExceptionHandler(ConstraintViolationException.class)
-  public ResponseEntity<Notification> handlerConstraintViolationException(Exception ex) {
-    log.error("Incorrect validations: {}", ex.getMessage());
+  public ResponseEntity<NotificationError> handleConstraintViolationException(ConstraintViolationException ex) {
+    List<ValidationError> validationErrors = ex.getConstraintViolations().stream()
+        .map(violation -> {
+          String fieldName = violation.getPropertyPath().toString(); // Ejemplo: "getDistinctSubsequences.subsequenceRequestDto.source"
+          String[] fieldParts = fieldName.split("\\.");
+          String cleanFieldName = fieldParts[fieldParts.length - 1]; // Obtiene solo el nombre del campo, "source"
+          return ValidationError.builder()
+              .field(cleanFieldName)
+              .message(violation.getMessage())
+              .build();
+        })
+        .collect(Collectors.toList());
 
-   return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-        .body(Notification.builder()
-            .message("Incorrect validations. " + ex.getMessage())
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+        .body(NotificationError.builder()
+            .message("Validation failed") // Usamos el mensaje construido sin el nombre del método o DTO
             .status(HttpStatus.BAD_REQUEST.value())
             .error(HttpStatus.BAD_REQUEST.getReasonPhrase())
+            .errors(validationErrors) // Se añade la lista de errores detallados
             .timestamp(Instant.now())
             .build());
   }
 
-  @ExceptionHandler(MethodArgumentNotValidException.class)
+ /* @ExceptionHandler(MethodArgumentNotValidException.class)
   public ResponseEntity<Notification> handlerMethodArgumentNotValidException(Exception ex) {
     log.error("Incorrect validations: {}", ex.getMessage());
     return ResponseEntity.status(HttpStatus.BAD_REQUEST)
@@ -78,13 +90,24 @@ public class HandlerExceptions {
             .error(HttpStatus.BAD_REQUEST.getReasonPhrase())
             .timestamp(Instant.now())
             .build());
+  }*/
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<Map<String, String>> handleValidationExceptions(MethodArgumentNotValidException ex) {
+    Map<String, String> errors = new HashMap<>();
+    ex.getBindingResult().getFieldErrors().forEach(error -> {
+      // Solo agrega el mensaje de error sin incluir el campo o nombre del DTO
+      errors.put(error.getField(), error.getDefaultMessage());
+    });
+
+    return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
   }
 
   @ExceptionHandler(Exception.class)
-  public ResponseEntity<Notification> handlerException(Exception ex) {
+  public ResponseEntity<NotificationError> handlerException(Exception ex) {
     log.error("Unexpected exception: {}", ex.getMessage(), ex);
     return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-        .body(Notification.builder()
+        .body(NotificationError.builder()
             .message("Unexpected error occurred. " + ex.getMessage())
             .status(HttpStatus.INTERNAL_SERVER_ERROR.value())
             .error(HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase())
